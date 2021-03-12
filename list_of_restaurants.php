a2170946bdbbddb9cfb04df63e89da628292e4b5
lexax123 <75291150+lexax123@users.noreply.github.com>
2021-03-12 12:17:38 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -1,4 +1,30 @@
-<?php
+
+
+
+
+
+<html>
+ <head>
+ <title>List of KL Restaurant</title>
+ </head>
+ <body>
+ <center>
+ <h1>List of KL Restaurant</h1>
+ <!--create table structure using HTML first-->
+ <table border="1">
+ <tr>
+ <th>Restaurant ID</th>
+<th>Restaurant Name</th>
+<th>Address</th>
+<th>Phone</th>
+ </tr>
+<tr>
+ <th>0</th>
+<th>Subway Restaurant</th>
+<th>Menara Standard Chartered, TPM</th>
+<th>03-22441234</th>
+ </tr>
+  <?php
  $serverName = "simplewebtp046600.database.windows.net"; 
  $connectionOptions = array(
  "Database" => "simpleweb",
@@ -33,31 +59,6 @@ if ($getResults == FALSE)
  sqlsrv_free_stmt($getResults);
 ?>
 
-
-
-
-
-<html>
- <head>
- <title>List of KL Restaurant</title>
- </head>
- <body>
- <center>
- <h1>List of KL Restaurant</h1>
- <!--create table structure using HTML first-->
- <table border="1">
- <tr>
- <th>Restaurant ID</th>
-<th>Restaurant Name</th>
-<th>Address</th>
-<th>Phone</th>
- </tr>
-<tr>
- <th>0</th>
-<th>Subway Restaurant</th>
-<th>Menara Standard Chartered, TPM</th>
-<th>03-22441234</th>
- </tr>
  </table>
  </center>
  </body>
