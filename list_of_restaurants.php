592688688985f4fda49143033b99c16bdd13b858
lexax123 <75291150+lexax123@users.noreply.github.com>
2021-03-12 12:16:40 +0800

    Update list_of_restaurants.php

--- list_of_restaurants.php
+++ list_of_restaurants.php
@@ -1,3 +1,42 @@
+<?php
+ $serverName = "simplewebtp046600.database.windows.net"; 
+ $connectionOptions = array(
+ "Database" => "simpleweb",
+ "Uid" => "simpleweb",
+"PWD" => "Simple@web");
+ 
+//Establishes the connection
+ $conn = sqlsrv_connect($serverName, $connectionOptions);
+ 
+if (!$conn)
+{
+ die("Error connection: ".sqlsrv_errors());
+ }
+ 
+ $tsql= "SELECT * FROM [dbo].[restaurant]";
+ $getResults= sqlsrv_query($conn, $tsql);
+ 
+if ($getResults == FALSE)
+{
+ die(sqlsrv_errors());
+}
+ 
+ while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) 
+{
+ echo "<tr>";
+ echo "<td>". $row['restaurant_id'] . "</td>";
+ echo "<td>". $row['restaurant_name'] ."</td>";
+ echo "<td>". $row['restaurant_address'] . "</td>";
+ echo "<td>". $row['restaurant_phone'] . "</td>";
+ echo "</tr>";
+ }
+ sqlsrv_free_stmt($getResults);
+?>
+
+
+
+
+
 <html>
  <head>
  <title>List of KL Restaurant</title>
